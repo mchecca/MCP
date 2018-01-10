785f3d34e6bd82f75a42237fbedcc815ec8c7ba1
Michael Checca <mchecca@gmail.com>
2018-01-09 20:23:12 -0500

    Add SmsUtil and ability to send SMS message to a specified phone number

---
+++ app/src/main/java/com/mchecca/mcp/Settings.java
@@ -0,0 +1,5 @@
+package com.mchecca.mcp;
+
+public class Settings {
+    public static String LOG_TAG = "com.mchecca.mcp";
+}
---
+++ app/src/main/java/com/mchecca/mcp/SmsUtil.java
@@ -0,0 +1,56 @@
+package com.mchecca.mcp;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.telephony.SmsManager;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import static com.mchecca.mcp.Settings.LOG_TAG;
+
+public class SmsUtil {
+    Activity activity;
+
+    public SmsUtil(Activity activity) {
+        this.activity = activity;
+    }
+
+    public boolean sendSms(String number, String message) {
+        try {
+            String SENT = LOG_TAG + "_SMS_SENT";
+            Intent myIntent = new Intent(SENT);
+            PendingIntent sentPI = PendingIntent.getBroadcast(activity.getApplicationContext(), 0, myIntent, 0);
+
+            SmsManager sms = SmsManager.getDefault();
+            ArrayList<String> messageParts = sms.divideMessage(message);
+            ArrayList<PendingIntent> sentPendingIntents = new ArrayList<PendingIntent>();
+            int messageCount = messageParts.size();
+
+            for (int i = 0; i < messageCount; i++) {
+                sentPendingIntents.add(sentPI);
+            }
+
+            sms.sendMultipartTextMessage(number, null, messageParts, sentPendingIntents, null);
+            addMessageToSent(number, message);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(LOG_TAG, "undefined Error: SMS sending failed");
+            return false;
+        }
+        return true;
+    }
+
+    private void addMessageToSent(String phoneNumber, String message) {
+        ContentValues sentSms = new ContentValues();
+        sentSms.put("address", phoneNumber);
+        sentSms.put("body", message);
+
+        ContentResolver contentResolver = activity.getContentResolver();
+        contentResolver.insert(Uri.parse("content://sms/sent"), sentSms);
+    }
+}
